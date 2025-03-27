8118fddd1012f05c0f121448e80e12583a3c8059
Il Nuovo <mirkobru2007@gmail.com>
2025-03-27 21:21:07 +0100

    created a navbar

--- homepage.php
+++ homepage.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>homepage</title>
 </head>
 <body>
+    <div class="navbar">
+        ciao
+    </div>
     WIP
 </body>
 </html>
